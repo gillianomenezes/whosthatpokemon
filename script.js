7bc26077a83b6e7b16d361915891f8e88fff26dd
gillianomenezes <gillianomenezes@gmail.com>
2016-03-16 00:06:36 -0300

    Verify if input text matches the pokemon name

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // JavaScript Document
+var number;
 
 function changeImage(){
-	var number =  Math.floor((Math.random() * 720) + 1);
+	number =  Math.floor((Math.random() * 720) + 1);
 	numberText = zeroFill(number, 3);
 	//alert(numberText + "  " + pokemon[number - 1]);
 	//document.getElementById("img_pokemon").src = "http://cdn.bulbagarden.net/upload/3/3d/" + numberText + pokemon[number - 1] + ".png";
@@ -19,6 +20,16 @@ function zeroFill( number, width )
     return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
   }
   return number + ""; // always return a string
+}
+
+function valueKeyPressed() {
+    var edValue = document.getElementById("inputName");
+    var inputNameValue = edValue.value.toLowerCase();
+            
+    if(inputNameValue == pokemon[number-1].toLowerCase())
+    {
+        alert("acertô, miserávi!!!");          
+    }    
 }
 
 var pokemon = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran?","Nidorina","Nidoqueen","Nidoran?","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"];
